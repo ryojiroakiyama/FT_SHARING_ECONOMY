8b26c31793acb42e91ed97f0fccfb5f36850e869
rakiyama <ryojiro25@gmail.com>
2022-07-29 17:29:00 +0900

    first test

--- integration-tests/rs/src/tests.rs
+++ integration-tests/rs/src/tests.rs
@@ -1,18 +1,23 @@
-use near_units::parse_near;
+use near_sdk::json_types::U128;
+use near_units::{parse_gas, parse_near};
 use serde_json::json;
 use workspaces::prelude::*;
+use workspaces::result::CallExecutionDetails;
 use workspaces::{network::Sandbox, Account, Contract, Worker};
 
-const WASM_FILEPATH: &str = "../../out/main.wasm";
+const DEFI_WASM_FILEPATH: &str = "../../out/main.wasm";
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    // initiate environemnt
     let worker = workspaces::sandbox().await?;
-    let wasm = std::fs::read(WASM_FILEPATH)?;
+
+    // deploy contracts
+    let wasm = std::fs::read(DEFI_WASM_FILEPATH)?;
     let contract = worker.dev_deploy(&wasm).await?;
 
     // create accounts
-    let owner = worker.root_account();
+    let owner = worker.root_account().unwrap();
     let alice = owner
         .create_subaccount(&worker, "alice")
         .initial_balance(parse_near!("30 N"))
@@ -20,47 +25,32 @@ async fn main() -> anyhow::Result<()> {
         .await?
         .into_result()?;
 
-    // begin tests
-    test_default_message(&alice, &contract, &worker).await?;
-    test_changes_message(&alice, &contract, &worker).await?;
-    Ok(())
-}
-
-async fn test_default_message(
-    user: &Account,
-    contract: &Contract,
-    worker: &Worker<Sandbox>,
-) -> anyhow::Result<()> {
-    let message: String = user
-        .call(&worker, contract.id(), "get_greeting")
-        .args_json(json!({}))?
-        .transact()
-        .await?
-        .json()?;
-
-    assert_eq!(message, "Hello".to_string());
-    println!("      Passed ✅ gets default message");
-    Ok(())
-}
-
-async fn test_changes_message(
-    user: &Account,
-    contract: &Contract,
-    worker: &Worker<Sandbox>,
-) -> anyhow::Result<()> {
-    user.call(&worker, contract.id(), "set_greeting")
-        .args_json(json!({"message": "Howdy"}))?
+    // Initialize contracts
+    contract
+        .call(&worker, "new")
+        .args_json(serde_json::json!({
+            "num_of_bikes": 5
+        }))?
         .transact()
         .await?;
 
-    let message: String = user
-        .call(&worker, contract.id(), "get_greeting")
-        .args_json(json!({}))?
-        .transact()
-        .await?
-        .json()?;
-
-    assert_eq!(message, "Howdy".to_string());
-    println!("      Passed ✅ changes message");
+    // begin tests
+     test_init(&owner, &contract, &worker).await?;
     Ok(())
 }
+
+ async fn test_init(
+     owner: &Account,
+     contract: &Contract,
+     worker: &Worker<Sandbox>,
+ ) -> anyhow::Result<()> {
+     let res: usize = owner
+         .call(&worker, contract.id(), "num_of_bikes")
+         .args_json(json!({}))?
+         .transact()
+         .await?
+         .json()?;
+     assert_eq!(res, 5);
+     println!("      Passed ✅ test_init");
+     Ok(())
+ }
